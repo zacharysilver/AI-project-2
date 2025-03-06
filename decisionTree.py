20ac6d2b0867ca8149959bd8bb32c36a22879995
Zack Silver <zacksilver@Zacks-MacBook-Air.local>
2025-03-05 22:13:43 -0600

    decision tree end wednesday

--- decisionTree.py
+++ decisionTree.py
@@ -3,43 +3,38 @@ import numpy as np
 data = pd.read_csv('data.csv')
 pd.set_option('display.max_columns', None)
 
-important_columns = ['track_popularity', 'danceability', 
-                     'energy', 'key', 'loudness', 'mode', 
-                     'speechiness', 'acousticness', 'instrumentalness', 
-                     'liveness', 'valence', 'tempo', 'duration_ms']
 
-data = data[important_columns]
+data = data[['track_popularity', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms']]
+
 data['track_popularity'] = (data['track_popularity'] >= 50).astype(int)
-        
-
 
 def bestEntropy(data):
     bestEntropy = 1
     bestColumn = ''
     bestValue = 0
     for column in data.columns:
-        if column != 'track_popularity':
+        if column != 'track_popularity': 
             sorted_data = data.sort_values(by=column)
             lt_true = 0
             lt_false = 0
-            gt_true = sum(data['track_popularity'])
-            gt_false = len(data) - gt_true
-            for i in range(len(sorted_data)-1):
-                if sorted_data.iloc[i]['track_popularity'] == 1:
-                    gt_true -= 1
+            ge_true = sum((data['track_popularity']).astype(int))
+            ge_false = len(data) - ge_true
+            for i in range(1, len(sorted_data)):
+                if sorted_data.iloc[i-1]['track_popularity']:
+                    ge_true -= 1
                     lt_true += 1
                 else:
-                    gt_false -= 1
+                    ge_false -= 1
                     lt_false += 1
-                if i + 1 < len(sorted_data) and sorted_data.iloc[i][column] == sorted_data.iloc[i + 1][column]:
+                if sorted_data.iloc[i-1][column] == sorted_data.iloc[i][column]:
                     continue
                 lt_entropy = 0
                 gt_entropy = 0
                 if lt_true != 0 and lt_false != 0:
                     lt_entropy = -((lt_true / (lt_true + lt_false)) * np.log2(lt_true / (lt_true + lt_false)) + (lt_false / (lt_true + lt_false)) * np.log2(lt_false / (lt_true + lt_false)))
-                if gt_true != 0 and gt_false != 0:
-                    gt_entropy = -((gt_true / (gt_true + gt_false)) * np.log2(gt_true / (gt_true + gt_false)) + (gt_false / (gt_true + gt_false)) * np.log2(gt_false / (gt_true + gt_false)))
-                entropy = (lt_true + lt_false) / len(sorted_data) * lt_entropy + (gt_true + gt_false) / len(sorted_data) * gt_entropy
+                if ge_true != 0 and ge_false != 0:
+                    gt_entropy = -((ge_true / (ge_true + ge_false)) * np.log2(ge_true / (ge_true + ge_false)) + (ge_false / (ge_true + ge_false)) * np.log2(ge_false / (ge_true + ge_false)))
+                entropy = (lt_true + lt_false) / len(sorted_data) * lt_entropy + (ge_true + ge_false) / len(sorted_data) * gt_entropy
                 if entropy < bestEntropy:
                     bestEntropy = entropy
                     bestColumn = column
@@ -52,14 +47,14 @@ def onelevel(data):
     print(column, val, entr)
     lt_true = data[data[column] < val]['track_popularity'].sum()
     lt_false = len(data[data[column] < val]) - lt_true
-    gt_true = data[data[column] >= val]['track_popularity'].sum()
-    gt_false = len(data[data[column] >= val]) - gt_true
-    print(lt_true, lt_false, gt_true, gt_false)
+    ge_true = data[data[column] >= val]['track_popularity'].sum()
+    ge_false = len(data[data[column] >= val]) - ge_true
+    print(lt_true, lt_false, ge_true, ge_false)
     return column, val
 
 def buildTree(data, depth):
     if depth ==0:
-        return bool(np.array(data['track_popularity']).mean()>0.5)
+        return bool(np.array(data['track_popularity']).mean()>=0.5)
     column, val = onelevel(data)
     left = data[data[column] < val]
 
@@ -76,17 +71,18 @@ def predict(tree, song):
         return predict(right, song)
 
 tree  = buildTree(data, 3)
+print(tree)
 
 true_positives, true_negatives, false_positives, false_negatives = 0, 0, 0, 0
 
 for i in range(len(data)):
     song = data.iloc[i]
     prediction = predict(tree, song)
-    if prediction and song['track_popularity']:
+    if prediction and song['track_popularity'] == 1:
         true_positives += 1
-    elif prediction and not song['track_popularity']:
+    elif prediction and not song['track_popularity'] ==1:
         false_positives += 1
-    elif not prediction and song['track_popularity']:
+    elif not prediction and song['track_popularity'] == 1:
         false_negatives += 1
     else:
         true_negatives += 1
@@ -96,3 +92,5 @@ print(f"True negatives: {true_negatives}")
 print(f"False positives: {false_positives}")
 print(f"False negatives: {false_negatives}")
 
+accuracy = (true_positives + true_negatives) / (true_positives + true_negatives + false_positives + false_negatives)
+print(f"Accuracy: {accuracy}")
